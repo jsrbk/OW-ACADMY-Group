32b80093727445c45fbf589a427f2df5143e5374
azizbek956 <azizbekshukurov11@gmail.com>
2021-09-19 13:48:55 +0500

    first commit

---
+++ Bootstrap web site/script.js
@@ -0,0 +1,23 @@
+var navTop = $("nav.navbar").offset().top;
+$(window).scroll(function () {
+    var currentScroll = $(window).scrollTop();
+    if ($(this).scrollTop()) {
+        $("#back_to_top").fadeIn();
+    } else {
+        $("#back_to_top").fadeOut();
+    }
+    if (Math.round($(this).scrollTop()) == $(document).height() - $(window).height()) {
+        $("#back_to_bottom").fadeOut();
+    } else {
+        $("#back_to_bottom").fadeIn();
+    }
+});
+$("#back_to_top").click(function () { 
+    $("html,body").animate({ scrollTop: 0 }, 0);
+});
+$("#back_to_bottom").click(function () { 
+    $("html,body").animate({ scrollTop: $(document).height() - $(window).height() }, 0);
+});
+$("#contact_form").click(function () { 
+    $(".contact_form").slideToggle(800);
+});
